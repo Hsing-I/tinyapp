db1144f5f09be3c346bd6ebe25517a0657096e40
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-18 21:49:25 +0000

    add form

--- express.server.js
+++ express.server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 8080;
 
@@ -9,6 +11,10 @@ const urlDatabase = {
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended:false})) 
+app.use(bodyParser.json());
+
+
 app.get("/", (req, res) => {
   //res.render('pages/index');
 });
@@ -18,12 +24,26 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, ()=>{
   console.log(`Example app listening on PROT ${PORT}!`);
 });
 
+function generateRandomString() {
+  return randomString = Math.random().toString(36).replace('0.', '').substring(0,6); 
+}
+
+console.log(generateRandomString());
