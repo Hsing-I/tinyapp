b8b12ee7d23be756c1d69e111f9be9d6958b0375
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-18 20:46:31 +0000

    add shorten url

--- express.server.js
+++ express.server.js
@@ -7,13 +7,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+  //res.render('pages/index');
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-})
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
 
 app.listen(PORT, ()=>{
   console.log(`Example app listening on PROT ${PORT}!`);
