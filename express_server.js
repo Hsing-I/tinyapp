e8cf94ad3a9976985d1bf801062d799b25e5c9ac
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-19 18:37:23 +0000

    edit functionality added

--- express_server.js
+++ express_server.js
@@ -51,6 +51,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.newURL;
+  res.redirect("/urls");
+});
+
 app.listen(PORT, ()=>{
   console.log(`Example app listening on PROT ${PORT}!`);
 });
